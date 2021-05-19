590ad4bb4c64ef07495a7b0da069baab8b9b4c37
Igor Solfa Romero <igorsolfaromero@gmail.com>
2021-05-19 12:59:14 -0300

    Select datas using knex.js.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var database = require("./database");
 
 // FUNCTION TO INSERT DATA
-function insertData(data){
-    var query = database.insert(data).into("games").then(d => {
+function insertData(data) {
+    database.insert(data).into("games").then(d => {
         console.log(d);
     }).catch(err => {
         console.log(err);
@@ -24,4 +24,54 @@ var dados = [
     }
 ]
 
-insertData(dados); // INSERT DATA
+// FUNCTION TO SELECT ALL DATA
+function selectData() {
+    database.select(["id", "name", "price"]).table("games").then(d => {
+        console.log(d);
+    }).catch(err => {
+        console.log(err);
+    })
+}
+
+// FUNCTION TO SELECT DATA AFTER INSERT
+function nestedQueries(data) {
+    database.insert(data).into("games").then(d => {
+        database.select(["id", "name", "price"]).table("games").then(d => {
+            console.log(d);
+        }).catch(err => {
+            console.log(err);
+        })
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+// FUNCTION TO SELECT WITH WHERE
+function selectDataWhere() {
+    database.select()
+        .where({ name: "Days Gone" })
+        .orWhere({ name: "Cyberpunk 2077" })
+        .whereRaw("price > 160")
+        .table("games")
+        .then(d => {
+            console.log(d);
+        }).catch(err => {
+            console.log(err);
+        })
+}
+
+/*
+// THIS DATABASE.RAW ALLOWS US TO WRITE A SQL COMMAND
+database.raw("SELECT * FROM games").then(d => {
+    console.log(d);
+}).catch(err => {
+    console.log(err);
+});
+*/
+
+
+
+// insertData(dados); // INSERT DATA
+// selectData(); // SELECT ALL DATA
+// nestedQueries(dados); // INSERT AND SELECT ALL DATA
+// selectDataWhere(); // SELECT DATA WITH WHERE
